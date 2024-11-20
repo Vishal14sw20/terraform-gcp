97eb76cc349f19b226d42590a3771992a2e5e805
Vishal <v.lohana@sevensenders.com>
2024-11-20 22:32:16 +0100

    test folders.

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  name          = "testing-cicdanu"
+  name          = "testing-cicdanu-prod"
   location      = "EU"
   storage_class = "STANDARD"
   versioning {
