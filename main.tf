3d595e3233ebe415a459228adaf77be531d435bc
Vishal <v.lohana@sevensenders.com>
2024-11-20 22:15:00 +0100

    added bucket

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project = "vishal-project-442115"
+  region  = "europe-west10"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name          = "testing-cicdanu"
+  location      = "EU"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
